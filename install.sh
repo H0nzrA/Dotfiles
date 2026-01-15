1dfa7704dadf3d04a245f63060372a9249a35acc
Teddy Andrianina <tedan.h0nzra@gmail.com>
2026-01-15 13:11:07 -0500

    Automatisation: check_dependencies.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 
-printf "=== ===	Dotfile configurations	=== ===\n"
+# Color section
+c_red="\033[31m"
+c_green="\033[32m"
+c_yellow="\033[33m"
+c_blue="\033[34m"
+c_magenta="\033[35m"
+c_cyan="\033[36m"
+c_reset="\033[0m"
 
+# Path section
 s_path=$(pwd)
+s_lib_path="$s_path/lib"
+s_nvim_path="$s_path/nvim"
+s_zsh_path="$s_path/zsh"
+
+# Lib section .sh file
+lib_check_d="$s_lib_path/check_dependencies.sh"
+
+f_backup="$HOME/.backup"
+
+function	check_dependencies()
+{
+	"$lib_check_d"
+}
+
+
+function	run()
+{
+	echo -e "${c_green}>>>>>>>>>>	Installing Dotfiles	<<<<<<<<<<${c_reset}"
+
+	check_dependencies
+}
+
+run
