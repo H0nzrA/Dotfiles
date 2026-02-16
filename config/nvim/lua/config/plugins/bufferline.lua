e498a923e50fb48c845cac41086b6ea6671202d1
Teddy Andrianina Rakotozafy <trakotoz@c1r1s10.42antananarivo.mg>
2026-02-16 17:40:02 +0300

    bufferline lazygit simple

--- config/nvim/lua/config/plugins/bufferline.lua
+++ config/nvim/lua/config/plugins/bufferline.lua
@@ -31,7 +31,7 @@ return {
 				show_buffer_close_icons = true,
 				show_close_icon = true,
 				persist_buffer_sort = true,
-				separator_style = "slant",
+				separator_style = "thin",
 				enforce_regular_tabs = false,
 				always_show_bufferline = true,
 				show_tab_indicators = true,
@@ -52,88 +52,25 @@ return {
 					reveal = {'close'}
 				},
 			},
-			highlights = {
-				fill = {
-					bg = 'NONE',
-				},
-				background = {
-					bg = 'NONE',
-				},
-				buffer_selected = {
-					bg = '#1e222a',
-					bold = true,
-					italic = true,
-				},
-				buffer_visible = {
-					bg = 'NONE',
-				},
-				close_button = {
-					bg = 'NONE',
-				},
-				close_button_selected = {
-					bg = '#1e222a',
-				},
-				close_button_visible = {
-					bg = 'NONE',
-				},
-				separator = {
-					fg = '#1e222a',
-					bg = 'NONE',
-				},
-				separator_selected = {
-					fg = '#1e222a',
-					bg = '#1e222a',
-				},
-				separator_visible = {
-					fg = '#1e222a',
-					bg = 'NONE',
-				},
-				indicator_selected = {
-					fg = '#61afef',
-					bg = '#1e222a',
-				},
-				modified = {
-					bg = 'NONE',
-				},
-				modified_selected = {
-					bg = '#1e222a',
-				},
-				modified_visible = {
-					bg = 'NONE',
-				},
-				diagnostic = {
-					bg = 'NONE',
-				},
-				diagnostic_selected = {
-					bg = '#1e222a',
-				},
-				diagnostic_visible = {
-					bg = 'NONE',
-				},
-			},
 		}
-
 		-- Tab: Next buffer
 		vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", {
 			noremap = true,
 			silent = true,
 			desc = "Next buffer",
 		})
-
 		-- Shift+Tab: Previous buffer
 		vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", {
 			noremap = true,
 			silent = true,
 			desc = "Previous buffer",
 		})
-
 		-- Space+tx: Close current buffer
 		vim.keymap.set("n", "<leader>tx", "<cmd>Bdelete!<CR>", {
 			noremap = true,
 			silent = true,
 			desc = "Close buffer",
 		})
-
 		-- Bonus: Space+tX: Close all other buffers
 		vim.keymap.set("n", "<leader>tX", "<cmd>BufferLineCloseOthers<CR>", {
 			noremap = true,
