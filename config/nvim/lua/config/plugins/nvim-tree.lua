e7793156d822c6c444a0ca7cffa4db152e9ee884
Teddy Andrianina Rakotozafy <trakotoz@student.42antananarivo.mg>
2026-03-29 13:21:02 +0300

    sexy file explorer

--- config/nvim/lua/config/plugins/nvim-tree.lua
+++ config/nvim/lua/config/plugins/nvim-tree.lua
@@ -1,25 +1,80 @@
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	config= function()
+	config = function()
 		local nvimtree = require("nvim-tree")
 
-		-- recommanded settings for nvim-tree documentation
+		-- Recommended settings for nvim-tree documentation
 		vim.g.loaded_netrw = 1
-		vim.g.loader_netrwPlugin= 1
+		vim.g.loader_netrwPlugin = 1
 
 		nvimtree.setup({
 			view = {
 				width = 50,
 				relativenumber = true,
 			},
+			renderer = {
+				add_trailing = false,
+				group_empty = false,
+				highlight_git = true,
+				full_name = false,
+				highlight_opened_files = "none",
+				root_folder_label = ":t",
+				indent_markers = {
+					enable = true,
+					inline_arrows = true,
+					icons = {
+						corner = "└",
+						edge = "│",
+						item = "│",
+						bottom = "─",
+						none = " ",
+					},
+				},
+				icons = {
+					webdev_colors = true, -- Enables the colored icons like Catppuccin
+					git_placement = "before",
+					padding = " ",
+					symlink_arrow = " ➜ ",
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+					glyphs = {
+						default = "󰈚",
+						symlink = "",
+						bookmark = "󰆤",
+						folder = {
+							arrow_closed = "⏵",
+							arrow_open = "⏷",
+							default = "󰉋",
+							open = "󰝰",
+							empty = "󰜌",
+							empty_open = "󰷏",
+							symlink = "",
+							symlink_open = "",
+						},
+						git = {
+							unstaged = "✗",
+							staged = "✓",
+							unmerged = "",
+							renamed = "➜",
+							untracked = "★",
+							deleted = "",
+							ignored = "◌",
+						},
+					},
+				},
+			},
 			filters = {
 				dotfiles = false,
 				custom = {},
 			},
 		})
-		local keymap = vim.keymap
 
+		local keymap = vim.keymap
 		keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
 		keymap.set("n", "<leader>o", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
 	end,
